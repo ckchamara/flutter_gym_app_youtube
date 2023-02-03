02c7c40c0bfcb7a2025361a3e6b1b329de9dbbc0
Chamara Dayarathne <ckchamara22@gmail.com>
2023-02-03 15:53:08 +0530

    47.00

--- lib/models/workout.dart
+++ lib/models/workout.dart
@@ -1,20 +1,28 @@
+import 'package:equatable/equatable.dart';
+
 import 'exercise.dart';
 
-class Workout {
+class Workout extends Equatable{
   final String? title;
   final List<Exercise> exercises;
 
   Workout({required this.title, required this.exercises});
 
-  factory Workout.fromJson(Map<String, dynamic> json){
-    List<Exercise> exercises=[];
+  factory Workout.fromJson(Map<String, dynamic> json) {
+    List<Exercise> exercises = [];
     int index = 0;
     int startTime = 0;
-    for(var ex in json['excercises'] as Iterable)  {
+    for (var ex in json['excercises'] as Iterable) {
       exercises.add(Exercise.fromJson(json, index, startTime));
       index++;
       startTime += exercises.last.prelude! + exercises.last.duration!;
     }
     return Workout(title: json['title'] as String?, exercises: exercises);
   }
+
+  Map<String, dynamic> toJson() => {'title': title, 'exercises': exercises};
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [title,exercises];
 }
