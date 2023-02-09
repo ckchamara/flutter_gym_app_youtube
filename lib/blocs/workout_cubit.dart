c1671fb9cf1388ef2ed0ba8406bcd11cbdfcd23b
Chamara Dayarathne <ckchamara22@gmail.com>
2023-02-09 16:10:05 +0530

    2.03

--- lib/blocs/workout_cubit.dart
+++ lib/blocs/workout_cubit.dart
@@ -1,5 +1,16 @@
-// import 'package:flutter_bloc/flutter_bloc.dart';
-//
-// class Workout extends Cubit<WorkoutState> {
-//
-// }
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_app_complete/models/workout.dart';
+
+import '../states/workout_states.dart';
+
+class WorkoutCubit extends Cubit<WorkoutState> {
+  // WorkoutCubit(super.initialState);
+  WorkoutCubit():super(const WorkoutInitial());
+
+  editWorkout(Workout workout, int index){
+    return emit(WorkoutEditing(workout, index));
+  }
+
+
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
- import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_app_complete/blocs/workout_cubit.dart';
 import 'package:flutter_bloc_app_complete/screens/home_screen.dart';
+import 'package:flutter_bloc_app_complete/states/workout_states.dart';
 
 import 'blocs/workouts_cubit.dart';
 import 'models/workout.dart';
 
 void main() => runApp(const WorkoutTime());
-
 
 class WorkoutTime extends StatelessWidget {
   const WorkoutTime({Key? key}) : super(key: key);
@@ -14,12 +15,12 @@ class WorkoutTime extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Workouts',
-      theme: ThemeData(
-          primaryColor: Colors.blue,
-          textTheme: const TextTheme(
-              bodyMedium: TextStyle(color: Color.fromARGB(255, 66, 74, 96)))),
-      home: BlocProvider<WorkoutsCubit>(
+        title: 'My Workouts',
+        theme: ThemeData(
+            primaryColor: Colors.blue,
+            textTheme: const TextTheme(
+                bodyMedium: TextStyle(color: Color.fromARGB(255, 66, 74, 96)))),
+        home: /*BlocProvider<WorkoutsCubit>(
         create: (BuildContext context) {
           WorkoutsCubit workoutsCubit = WorkoutsCubit();
           if(workoutsCubit.state.isEmpty){
@@ -35,7 +36,33 @@ class WorkoutTime extends StatelessWidget {
             return HomeScreen();
           },
         ),
-      ),
-    );
+      ),*/
+            MultiBlocProvider(
+                providers: [
+              BlocProvider<WorkoutsCubit>(create: (BuildContext context) {
+                WorkoutsCubit workoutsCubit = WorkoutsCubit();
+                if (workoutsCubit.state.isEmpty) {
+                  print("Loading json - state is empty");
+                  workoutsCubit.getWorkouts();
+                } else {
+                  print("state not empty");
+                }
+                return workoutsCubit;
+              }),
+
+
+              BlocProvider<WorkoutCubit>(
+                  create: (BuildContext context) => WorkoutCubit())
+            ],
+                child: BlocBuilder<WorkoutCubit, WorkoutState>(
+                  builder: (context, state) {
+                    if (state is WorkoutInitial) {
+                      return HomeScreen();
+                    } else if (state is WorkoutEditing) {
+                      return Container();
+                    }
+                    return Container();
+                  },
+                )));
   }
 }
