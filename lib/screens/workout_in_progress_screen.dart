04ebb08492a1e4026f72afb6bf977b50c5e4d66a
Chamara Dayarathne <ckchamara22@gmail.com>
2023-02-13 16:06:50 +0530

    5 13

--- lib/screens/workout_in_progress_screen.dart
+++ lib/screens/workout_in_progress_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_app_complete/blocs/workout_cubit.dart';
+import 'package:flutter_bloc_app_complete/helpers.dart';
 import 'package:flutter_bloc_app_complete/models/exercise.dart';
 import 'package:flutter_bloc_app_complete/states/workout_states.dart';
 
@@ -28,23 +29,70 @@ class WorkoutInProgressScreen extends StatelessWidget {
       return {
         "workoutTitle": workout.title,
         "workoutProgress": workoutElapsed / workoutTotal,
+        "workoutElapsed": workoutElapsed,
+        "totalExercise": workout.exercises.length,
+        "currentExerciseIndex": exercise.index!.toDouble(),
+        "workoutRemainingTime": workoutTotal - workoutElapsed,
+        "exerciseRemaining": exerciseRemaining,
+        "exerciseProgress": exerciseElapsed / exerciseTotal,
+        "isPrelude": isPrelude
       };
     }
 
     return BlocConsumer<WorkoutCubit, WorkoutState>(
         builder: (context, state) {
+          final stats = _getStats(state.workout!, state.elapsed!);
           return Scaffold(
-            appBar: AppBar(
-              title: Text(state.workout!.title.toString()),
-              leading: BackButton(
-                onPressed: () =>
-                    BlocProvider.of<WorkoutCubit>(context).goHome(),
+              appBar: AppBar(
+                title: Text(state.workout!.title.toString()),
+                leading: BackButton(
+                  onPressed: () =>
+                      BlocProvider.of<WorkoutCubit>(context).goHome(),
+                ),
               ),
-            ),
-            body: Center(
-              child: Text("${state.elapsed}"),
-            ),
-          );
+              body: Container(
+                padding: const EdgeInsets.all(32),
+                child: Column(
+                  children: [
+                    LinearProgressIndicator(
+                      backgroundColor: Colors.blue[100],
+                      minHeight: 10,
+                      value: stats['workoutProgress'],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 30),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(formatTime(stats['workoutElapsed'], true)),
+                          Text(
+                              '-${formatTime(stats['workoutRemaining'], true)}')
+                        ],
+                      ),
+                    ),
+                    const Spacer(),
+                    InkWell(
+                      child: Stack(
+                        children: [
+                          Center(
+                            child: SizedBox(
+                              height: 220,
+                              width: 220,
+                              child: CircularProgressIndicator(
+                                valueColor: AlwaysStoppedAnimation<Color>(
+                                  stats['isPrelude']?Colors.red : Colors.blue
+                                ),
+                                strokeWidth: 25,
+                                value: stats['exerciseProgress'],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ));
         },
         listener: (context, state) {});
   }
